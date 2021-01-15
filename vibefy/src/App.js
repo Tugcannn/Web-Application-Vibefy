62da287a4e6653a7b0a6957b0e956a66bcfccf97
Tugcannn <tugcanhsr@hotmail.com>
2021-01-16 00:09:57 +0300

    commit 2

--- vibefy/src/App.js
+++ vibefy/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react' ;
+import Header from "./components/Header" ;
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vibefy</title>
+    <link rel="icon" href="../assets/images/svgs/headphones_head.svg" type="image/svg" />
+    <link rel="stylesheet" href="../css/style.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <style>
+    .header-right a:hover {'{'}
+        background-color: #FF00EB;
+        color: black;
+        {'}'}
+
+.content .fas {'{'}
+        font-size: 1.4375rem;
+        background: #FF00EB;
+        height: 45px;
+        width: 45px;
+        line-height: 45px;
+        text-align: center;
+        border-radius: 50%;
+        transition: 0.3s;
+        cursor: pointer;
+        {'}'}
+
+body::-webkit-scrollbar-thumb {'{'}
+        background: #FF00EB;
+        {'}'}
+    </style>
+    
+    <style>="background-color:#141418;"</style>
+    <header id="header"></header>
+    <nav class="navbar"></nav>
+    <main>
+        <img src="../assets/images/png/music.jpg" style="display: block; margin-left: auto; margin-right: auto;" />
+    </main>
+    <footer id="my_footer"></footer>
+
     </div>
+    
   );
 }
 
---
+++ vibefy/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>LAY LAY LAY SALDIR GALATAAAAAAAAASARAY</h1>
+            </div>
+        )
+    }
+}
+export default Header;
