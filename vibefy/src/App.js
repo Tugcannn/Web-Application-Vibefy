4af3976ea8f3c3e718417f8fee3e0d3a9f9a2d4e
Tuna <kuantuna.tr@gmail.com>
2021-01-16 04:26:16 +0300

    Started

--- vibefy/src/App.js
+++ vibefy/src/App.js
@@ -1,48 +1,14 @@
 import React, { Component } from 'react' ;
 import Header from "./components/Header" ;
+import Jumbo from "./components/Jumbo";
 import './App.css';
 
 function App() {
   return (
     <div>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vibefy</title>
-    <link rel="icon" href="../assets/images/svgs/headphones_head.svg" type="image/svg" />
-    <link rel="stylesheet" href="../css/style.css" />
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <style>
-    .header-right a:hover {'{'}
-        background-color: #FF00EB;
-        color: black;
-        {'}'}
-
-.content .fas {'{'}
-        font-size: 1.4375rem;
-        background: #FF00EB;
-        height: 45px;
-        width: 45px;
-        line-height: 45px;
-        text-align: center;
-        border-radius: 50%;
-        transition: 0.3s;
-        cursor: pointer;
-        {'}'}
-
-body::-webkit-scrollbar-thumb {'{'}
-        background: #FF00EB;
-        {'}'}
-    </style>
-    
-    <style>="background-color:#141418;"</style>
-    <header id="header"></header>
-    <nav class="navbar"></nav>
-    <main>
-        <img src="../assets/images/png/music.jpg" style="display: block; margin-left: auto; margin-right: auto;" />
-    </main>
-    <footer id="my_footer"></footer>
-
+      <Header/>
+      <Jumbo/>
     </div>
-    
   );
 }
 
--- vibefy/src/components/Header.js
+++ vibefy/src/components/Header.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-class Header extends Component {
-    render() {
-        return (
-            <div>
-                <h1>LAY LAY LAY SALDIR GALATAAAAAAAAASARAY</h1>
-            </div>
-        )
-    }
+function Header() {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">VIBEFY</Navbar.Brand>
+            <Nav className="mr-auto"></Nav>
+            <Nav className="ms-auto">
+                <NavDropdown title="Moods" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">I am pretty very happpyyy!</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.2">Naa, I am so sad actually :/</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.3">I am angry, just play music</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#home">Login</Nav.Link>
+                <Nav.Link href="#features">Register</Nav.Link>
+            </Nav>
+        </Navbar>
+    );
 }
+
 export default Header;
